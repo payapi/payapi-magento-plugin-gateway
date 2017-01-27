6d7e35a0c1b8319b1485c015ed1203b320a76397
Francisco <francisco@payapi.io>
2017-01-27 15:47:16 +0100

    Remove payapi shipping method

---
+++ Payapi/CheckoutPayment/Model/Config/AllActiveMethods.php
@@ -0,0 +1,32 @@
+<?php
+/**
+ * Copyright © 2016 Magento. All rights reserved.
+ * See COPYING.txt for license details.
+ */
+namespace Payapi\CheckoutPayment\Model\Config;
+
+class AllActiveMethods extends \Magento\Shipping\Model\Config\Source\Allmethods
+{
+    /**
+     * @param \Magento\Framework\App\Config\ScopeConfigInterface $scopeConfig
+     * @param \Magento\Shipping\Model\Config $shippingConfig
+     */
+    public function __construct(
+        \Magento\Framework\App\Config\ScopeConfigInterface $scopeConfig,
+        \Magento\Shipping\Model\Config $shippingConfig
+    ) {
+        parent::__construct($scopeConfig,$shippingConfig);
+    }
+
+    /**
+     * Return array of active carriers.
+     *
+     * @return array
+     */
+    public function toOptionArray()
+    {
+        return parent::toOptionArray(true);
+    }
+
+}
+?>
