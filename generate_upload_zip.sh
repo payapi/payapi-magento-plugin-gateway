b7fb87080b15152ad222ca87522a3cea584fbe56
Francisco <francisco@payapi.io>
2017-02-02 17:44:55 +0100

    Remove generation script

--- generate_upload_zip.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-#
-echo "PAYAPI - MAGENTO 2. GENERATE NEW RELEASE AND UPLOAD TO GCLOUD"
-
-find . -name '*.DS_Store' -type f -delete
-zip -r payapi-magento-plugin.zip Payapi/* i18n/*
-gsutil cp payapi-magento-plugin.zip gs://public0/payapi-magento-plugin/latest
-echo "Done"
