b17e9b990d05d571f8583b661ca5d76b34c4d758
Francisco <francisco@payapi.io>
2017-10-19 13:18:42 +0200

    Refactor Plugin config file

--- Block/Adminhtml/PayapiPluginConfiguration.php
+++ Block/Adminhtml/PayapiPluginConfiguration.php
@@ -4,91 +4,94 @@ namespace Payapi\CheckoutPayment\Block\Adminhtml;
 
 use Payapi\PaymentsSdk\payapiSdk;
 
-class PayapiPluginConfiguration extends \Magento\Framework\View\Element\Template
-{
+class PayapiPluginConfiguration extends \Magento\Framework\View\Element\Template {
 
-    public function __construct(
-        \Magento\Framework\View\Element\Template\Context $context,
-        \Magento\Payment\Helper\Data $paymentHelper,
-        \Magento\Framework\HTTP\PhpEnvironment\RemoteAddress $remoteAddress,
-        \Payapi\CheckoutPayment\Model\Config\AllActiveShippingMethods $allShippingMethods,
-        \Payapi\CheckoutPayment\Logger\Logger $logger,
-        array $data = []
-    ) {
-        $this->logger = $logger;
-        $this->paymentHelper = $paymentHelper;
-        $this->remoteAddress = $remoteAddress;
-        $this->allShippingMethods = $allShippingMethods;
-        $this->sdk = false;
-        parent::__construct($context, $data);
-    }
+	public function __construct(
+		\Magento\Framework\View\Element\Template\Context $context,
+		\Magento\Payment\Helper\Data $paymentHelper,
+		\Magento\Framework\HTTP\PhpEnvironment\RemoteAddress $remoteAddress,
+		\Payapi\CheckoutPayment\Model\Config\AllActiveShippingMethods $allShippingMethods,
+		\Payapi\CheckoutPayment\Logger\Logger $logger,
+		array $data = []
+	) {
+		$this->logger = $logger;
+		$this->paymentHelper = $paymentHelper;
+		$this->remoteAddress = $remoteAddress;
+		$this->allShippingMethods = $allShippingMethods;
+		$this->sdk = false;
+		parent::__construct($context, $data);
+	}
 
-    public function checkPayApiConfiguration()
-    {
-        $paymentMethod                   = $this->paymentHelper->getMethodInstance("payapi_checkoutpayment_secure_form_post");
-        $this->payapiApiKey              = $paymentMethod->getConfigData('payapi_api_key');
-        $this->payapiPublicId            = $paymentMethod->getConfigData('payapi_public_id');
-        $this->instantBuyDefaultShipping = $paymentMethod->getConfigData('instantbuy_shipping_method');
-        $this->isInstantBuyEnabled       = $paymentMethod->getConfigData('instantbuy_enabled');
-        $this->isStaging                 = $paymentMethod->getConfigData('staging');
-        $this->isEnabled                 = $paymentMethod->getConfigData('active');
+	public function checkPayApiConfiguration() {
+		$paymentMethod = $this->paymentHelper->getMethodInstance("payapi_checkoutpayment_secure_form_post");
+		$this->payapiApiKey = $paymentMethod->getConfigData('payapi_api_key');
+		$this->payapiPublicId = $paymentMethod->getConfigData('payapi_public_id');
+		$this->instantBuyDefaultShipping = $paymentMethod->getConfigData('instantbuy_shipping_method');
+		$this->isInstantBuyEnabled = $paymentMethod->getConfigData('instantbuy_enabled');
+		$this->isStaging = $paymentMethod->getConfigData('staging');
+		$this->isEnabled = $paymentMethod->getConfigData('active');
 
-        $checkOk = $this->isEnabled && isset($this->payapiPublicId) && isset($this->payapiApiKey) && isset($this->instantBuyDefaultShipping) && is_string($this->instantBuyDefaultShipping) && strlen($this->instantBuyDefaultShipping) > 0 && $this->allShippingMethods->contains($this->instantBuyDefaultShipping);
-        if ($checkOk){            
-            $config = ["debug" => $this->isStaging == '1'];
-            if(!$this->sdk){
-                $this->sdk = new payapiSdk($config, 'magento');
-                //TODO MOVE TO ADMIN, WHEN PAYAPI DATA IS SAVED. REPLACE FOR SETTINGS()
-            }
-            // $staging = $this->isStaging == '1'; 
-            // TODO ADD FIRST PARAMETER OF SETTINGS BOOLEAN IS STAGING/PRODUCTION
-            $resp = $this->sdk->settings($this->payapiPublicId, $this->payapiApiKey);//, true); TO REFRESH                        
-            return $resp['code'] === 200;
-        }
+		$checkOk = $this->isEnabled && isset($this->payapiPublicId) && isset($this->payapiApiKey) && isset($this->instantBuyDefaultShipping) && is_string($this->instantBuyDefaultShipping) && strlen($this->instantBuyDefaultShipping) > 0 && $this->allShippingMethods->contains($this->instantBuyDefaultShipping);
+		if ($checkOk) {
+			$config = ["debug" => true];
 
-        return false;
-    }
+			if (!$this->sdk) {
+				$this->sdk = new payapiSdk($config, 'magento', false);
+			}
 
-    public function getPublicId()
-    {
-        return $this->payapiPublicId;
-    }
+			$refreshMerchantSettings = $this->getRequest()->getActionName() === "edit" && $this->getRequest()->getModuleName() === "admin" && $this->getRequest()->getParam('section') === "payment";
 
-    public function getApiKey()
-    {
-        return $this->payapiApiKey;
-    }
+			if ($refreshMerchantSettings) {
+				$resp = $this->sdk->settings($this->isStaging == '1', $this->payapiPublicId, $this->payapiApiKey);
+			} else {
+				$resp = $this->sdk->settings();
+				if ($resp['code'] != 200) {
+					$resp = $this->sdk->settings($this->isStaging == '1', $this->payapiPublicId, $this->payapiApiKey);
+				}
+			}
 
-    public function getIsInstantBuyEnabled(){
-        return $this->isInstantBuyEnabled;
-    }
+			return $resp['code'] === 200;
+		}
 
-    public function getIsStaging()
-    {
-        return $this->isStaging;
-    }
+		return false;
+	}
 
-    public function getVisitorIp($checkParams = true)
-    {
-        $ipaddress = '';
-        $paramIp   = $this->getRequest()->getQueryValue('ip');
-        if ($checkParams && $paramIp) {
-            return $paramIp;
-        } else {
-            $ipaddress = $this->getRequest()->getClientIp();
-        }
-        return $ipaddress;
-    }
+	public function getPublicId() {
+		return $this->payapiPublicId;
+	}
 
-    public function getPhpSdk(){
-        return $this->sdk;
-    }
+	public function getApiKey() {
+		return $this->payapiApiKey;
+	}
 
-    public function getPartner() {        
-        $partnerId = 'somepay'; //'payapi'
-        if (isset($this->sdk)) {
-            return $this->sdk->plugin($partnerId);
-        }
-        return false;
-    }
+	public function getIsInstantBuyEnabled() {
+		return $this->isInstantBuyEnabled;
+	}
+
+	public function getIsStaging() {
+		return $this->isStaging;
+	}
+
+	public function getVisitorIp($checkParams = true) {
+		$ipaddress = '';
+		$paramIp = $this->getRequest()->getQueryValue('ip');
+		if ($checkParams && $paramIp) {
+			return $paramIp;
+		} else {
+			$ipaddress = $this->getRequest()->getClientIp();
+		}
+		return $ipaddress;
+	}
+
+	public function getPhpSdk() {
+		return $this->sdk;
+	}
+
+	public function getPartner() {
+		$partnerId = 'payapi'; //'payapi'
+		if (isset($this->sdk)) {
+			return $this->sdk->branding($partnerId);
+		}
+		return false;
+	}
 }
